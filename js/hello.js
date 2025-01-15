96cd163508ce1d465cce7ae20b7243af8c8c0cb8
spawar3345 <sanketpawar1200@gmail.com>
2025-01-15 10:55:59 +0530

    updated hello world page

--- js/hello.js
+++ js/hello.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello World! by Sanket";
+  greeting.innerHTML = "Hello World! by Sanket Pawar!";
 
   element.appendChild(greeting);
 
